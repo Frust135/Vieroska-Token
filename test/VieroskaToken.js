9682211624bd625de0c8fb0fb04d8d7de514a610
Frust <santiagosaav.99@gmail.com>
2022-01-02 20:05:09 -0300

    Test for allocate the initial supply, name, symbol and standard

--- test/VieroskaToken.js
+++ test/VieroskaToken.js
@@ -1,13 +1,29 @@
 var VieroskaToken = artifacts.require("VieroskaToken");
 
 contract('VieroskaToken', function(accounts){
+    var tokenInstance;
 
-    it('Sets the total supply upon deployment', function(){
+    it('Initialize the contract with the correct values', function(){
+        return VieroskaToken.deployed().then(function(instance){
+            tokenInstance = instance;
+            return tokenInstance.name();
+        }).then(function(name){
+            assert.equal(name, 'Vieroska', 'Has the correct NAME');
+            return tokenInstance.symbol();
+        }).then(function(symbol){
+            assert.equal(symbol, 'VSK', 'Has the correct SYMBOL')
+        });
+    });
+
+    it('Allocates the initial supply upon deployment', function(){
         return VieroskaToken.deployed().then(function(instance){
             tokenInstance = instance;
             return tokenInstance.totalSupply();
         }).then(function(totalSupply){
-            assert.equal(totalSupply.toNumber(), 500, 'Sets the total supply to 500')
+            assert.equal(totalSupply.toNumber(), 500, 'Sets the total supply to 500');
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(adminBalance){
+            assert.equal(adminBalance.toNumber(), 500, 'It allocates the initial supply to the admin account');
         })
     });
-})
+});
